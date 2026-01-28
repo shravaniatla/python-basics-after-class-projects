3f627922a0db188eca49a6d3217144c770935d18
shravaniatla <shravani.atla13@gmail.com>
2026-01-28 17:15:52 +0530

    Add files via upload

---
+++ sorting birthday dates.py
@@ -0,0 +1,10 @@
+birthday1 = "12-03-2005"
+birthday2 = "25-07-2006"
+birthday3 = "01-01-2004"
+birthday4 = "18-10-2003"
+birthday5 = "30-12-2005"
+print(birthday1)
+print(birthday5)
+print(birthday2)
+print(birthday3)
+print(birthday4)
