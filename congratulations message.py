0347f2e867634c5aa5c0b00642c78724a984dd0c
shravaniatla <shravani.atla13@gmail.com>
2026-01-28 17:38:10 +0530

    Add files via upload

---
+++ congratulations message.py
@@ -0,0 +1,16 @@
+Name = "good going"
+
+#length of the string
+print(len(Name))
+
+#accessing a paticular character in the string using index number
+print(Name[0])
+
+#slicing
+print(Name[2:5])
+
+#reverse the string
+print(Name[::-1])
+
+
+
